64482759523de9764aab72e9ad83cc9a3cb8d062
joshkay47 <j_kalenga@coloradocollege.edu>
2021-12-01 15:46:46 -0700

    fixed error on email checking

--- login-page.js
+++ login-page.js
@@ -8,12 +8,31 @@ loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
     e.preventDefault();
     // Get the values input by the user in the form fields
-    const email = loginForm.username.value;
+    const email = loginForm.email.value;
 
     if (email.includes("coloradocollege"))  {
-        // If the credentials are valid, show an alert box and reload the page
-        alert("You have successfully logged in.");
-        location.reload();
+        // define the callAPI function that takes email as parameters
+        var callAPI = (email)=>{
+            // instantiate a headers object
+            var myHeaders = new Headers();
+            // add content type header to object
+            myHeaders.append("Content-Type", "application/json");
+            // using built in JSON utility package turn object to string and store in a variable
+            var raw = JSON.stringify({"email":email});
+            // create a JSON object with parameters for API call and store in a variable
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            // make API call with parameters and use promises to get response
+            fetch("https://y1gibi1ksk.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+            .then(response => response.text())
+            .then(result => alert(JSON.parse(result).body))
+            .catch(error => console.log('error', error));
+        }
+        
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
