5ee4a1e4f075bcfc8ded646aa1057f1147c27552
Jill Pollard <j_pollard@coloradocollege.edu>
2021-12-01 14:53:39 -0700

    successfully connected front and backend through API –– only issue is I put js in the html.

--- login-page.js
+++ login-page.js
@@ -2,6 +2,7 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
 // When the login button is clicked, the following code is executed
 loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
