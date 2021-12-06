2df049a33638be0680c45e440549156db581bef9
Jill Pollard <j_pollard@coloradocollege.edu>
2021-12-05 20:59:41 -0700

    SignIn page is displayed. Still working on routing.

--- college-connect/src/App.js
+++ college-connect/src/App.js
@@ -3,23 +3,24 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import SignIn from './components/SignIn';
-import Information from './components/Information';
+import SignIn from './SignIn';
 import ProTip from './ProTip';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-// function App(){
-//   return(
-//       <Routes>
-//         <Route path="/" component={SignIn} exact/>
-//         <Route path="/information" component={Information} />
-//       </Routes>
-//   );
-// }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        CollegeConnect
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 export default function App() {
   return (
-    <Information />
+   <SignIn />
   );
-  }
-// export default App;
+}
--- college-connect/src/index.js
+++ college-connect/src/index.js
@@ -4,20 +4,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
-import {BrowserRouter} from 'react-router-dom';
-
-// ReactDOM.render(
-//   <ThemeProvider theme={theme}>
-//     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-//     <CssBaseline />
-//     <App />
-//   </ThemeProvider>,
-//   document.querySelector('#root'),
-// );
 
 ReactDOM.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root'),
 );
