4f4ab7244e8f8fc62bfa1dba09d7128034734382
Nicholas Bohm <nickbohm555@gmail.com>
2021-12-06 14:23:07 -0700

    DS changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Box from '@mui/material/Box';
 import Information from './components/Information';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 // import { RouterOutlined } from '@mui/icons-material';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-    <Router>
+      <Router>
         <div>
           <Switch>
-              <Route exact path='/' component={SignIn} />
-              <Route path='/information' component={Information} />
-              <Route path='/home' component={Home} />
+            <Route exact path='/' component={SignIn} />
+            <Route path='/information' component={Information} />
+            <Route path='/home' component={Home} />
           </Switch>
         </div>
       </Router>
@@ -24,3 +24,5 @@ class App extends Component {
   }
 }
 export default App;
+
+// change
