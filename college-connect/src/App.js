c24552e5e5f214336f165ede9e461b198ebf2aaa
Jill Pollard <j_pollard@coloradocollege.edu>
2021-12-06 09:56:46 -0700

    Information page displaying. Working on routing still.

--- college-connect/src/App.js
+++ college-connect/src/App.js
@@ -2,25 +2,60 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import SignIn from './SignIn';
+//import Link from '@mui/material/Link';
+import SignIn from './components/SignIn';
+import Information from './components/Information';
+import Home from './components/Home';
 import ProTip from './ProTip';
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { RouterOutlined } from '@mui/icons-material';
+import {Component} from 'react';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        CollegeConnect
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// class App extends Component {
+//   render() {
+//     return (
+//     <Router>
+//         <div>
+//           <h2>Welcome to React Router Tutorial!!!</h2>
+//           <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//           <ul className="navbar-nav mr-auto">
+//           <li><Link to={'https://www.facebook.com'} className="nav-link"> facebook </Link></li>
+//             <li><Link to={'/'} className="nav-link"> Home </Link></li>
+//             <li><Link to={'/information'} className="nav-link">Information</Link></li>
+//             <li><Link to={'/signin'} className="nav-link">Sign In</Link></li>
+//           </ul>
+//           </nav>
+//           <hr />
+//           <Routes>
+//               <Route exact path='/' component={Home} />
+//               <Route path='/information' component={Information} />
+//               <Route path='/signin' component={SignIn} />
+//           </Routes>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+// export default function App(){
+//   return(
+//     <div className="App">
+//         <Routes>
+//           <Route path="/" component={SignIn} />
+//           <Route path="/information" component={Information} />
+//           <Route path="/home" component={Home} />
+//         </Routes>
+//     </div>
+//     );
+// }
 
 export default function App() {
   return (
-   <SignIn />
+    <Information />
   );
-}
+  }
+
+//export default App;
--- college-connect/src/index.js
+++ college-connect/src/index.js
@@ -4,12 +4,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
+import {BrowserRouter} from 'react-router-dom';
 
+
+// ReactDOM.render(
+//   <ThemeProvider theme={theme}>
+//     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+//     <CssBaseline />
+//     <App />
+//   </ThemeProvider>,
+//   document.querySelector('#root'),
+// );
+
+// rendering a BrowserRoute object
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-  document.querySelector('#root'),
+  //<BrowserRouter>
+  <App />,
+ // </BrowserRouter>,
+  document.getElementById('app')
 );
