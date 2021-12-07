3cd306d9450185583e351b9c97aad93317a5d72b
joshkay47 <74512484+joshkay47@users.noreply.github.com>
2021-12-06 21:59:48 -0800

    Updating main branch to match Jill branch

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Information from './components/Information';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import NotFound from './components/NotFound';
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+// import { RouterOutlined } from '@mui/icons-material';
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <Switch>   
+              <Route exact path='/' component={SignIn} /> 
+              <Route path='/signin' component={Information} />
+              <Route path='/home' component={Home} />
+              <Route path='*' exact={true} component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  } 
+}
+export default App;
