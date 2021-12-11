d12eb181f177de175ecf0e2841661478a44d22e4
joshkay47 <74512484+joshkay47@users.noreply.github.com>
2021-12-11 11:58:49 -0700

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,7 +2,7 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
+import awsExports from '../aws-exports.js';
 import React from 'react';
 Amplify.configure(awsExports);
 
