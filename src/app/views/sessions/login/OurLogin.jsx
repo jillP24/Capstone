3f0a9aabf3a5d1b7ec85be94b0b4af9675a3f8de
Jill Pollard <j_pollard@coloradocollege.edu>
2021-12-14 10:06:51 -0700

    got login working!!!

--- src/app/views/sessions/login/OurLogin.jsx
+++ src/app/views/sessions/login/OurLogin.jsx
@@ -1,22 +1,67 @@
+//import '../App.css';
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut, AmplifySignUp } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+//import awsconfig from '../aws-exports';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 //import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from '../../../../aws-exports.js';
 import React from 'react';
 Amplify.configure(awsExports); 
-//import DashboardRoutes from './views/dahsboard'
 
-// we want it to return this entire application ... 
-// is that possible??
-function App({ signOut, user }) {
-  return (
-    <div>
-      hello
-    </div>
-   // want to call ./default/dashboard
-   // Route::get("edit_contact/{id}",'ContactController@edit')->name('edit_contact');
-  );    
+
+const formFields =
+  [
+    {
+      type: "email",
+      required: true,
+    },
+    {
+      type: "password",
+      required: true,
+    },
+    {
+      type: "given_name",
+      label: 'First Name',
+      required: true,
+    },
+    {
+      type: "family_name",
+      label: "Last Name",
+      required: true,
+    },
+    {
+      type: "custom:class",
+      label: "Graduating class",
+      required: true,
+      placeholder: "e.g. 2023"
+    }
+  ];
+
+
+const App = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App">
+          <div>Hello, {user.attributes.given_name}</div>
+          <AmplifySignOut />
+      </div>
+    ) : (
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignIn headerText="College Connect"/>
+        <AmplifySignUp formFields={formFields} usernameAlias="email" slot="sign-up" />
+      </AmplifyAuthenticator>
+  );
 }
-export default withAuthenticator(App); 
+
+export default App;
  
